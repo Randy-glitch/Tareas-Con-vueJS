3e5e401ca52034192296575038ccc861f83a155e
Randy <84881845+Randy-glitch@users.noreply.github.com>
2022-03-02 13:45:17 -0400

    Add files via upload

---
+++ Tareas VueJs/app.js
@@ -0,0 +1,6 @@
+function Confirmar() {
+    document.querySelector('.confirmar').style = "display:block"
+    setTimeout(() => {
+        document.querySelector('.confirmar').style = "display:none"
+    }, 4000);
+}
